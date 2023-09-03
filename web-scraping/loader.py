87c2a72ecdc68720f68b575690c51b704dd8eaef
vonewman <vonewman7@gmail.com>
2023-09-03 15:18:12 +0000

    initial commit of the AI projects

---
+++ web-scraping/loader.py
@@ -0,0 +1,13 @@
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import BeautifulSoupTransformer
+
+# Load HTML
+loader = AsyncChromiumLoader(["https://www.wsj.com"])
+html = loader.load()
+
+# Transform
+bs_transformer = BeautifulSoupTransformer()
+docs_transformed = bs_transformer.transform_documents(html, tags_to_extract=["span"])
+
+# Result
+print(docs_transformed[0].page_content[0:500])
