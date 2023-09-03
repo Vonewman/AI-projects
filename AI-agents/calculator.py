404cd5a60b42ad6971120cfc9c1861f7d95b0e5b
vonewman <vonewman7@gmail.com>
2023-09-03 20:21:58 +0000

    add calculator.py

---
+++ AI-agents/calculator.py
@@ -0,0 +1,18 @@
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+from decouple import config
+
+
+llm = OpenAI(temperature=0.6)
+
+tools = load_tools(['serpapi', 'llm-math'], llm=llm)
+
+agent = initialize_agent(
+	tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+print(agent.agent.llm_chain.prompt.template)
+
+agent.run("My monthly salary is 10000 KES, if i work for 10 months. How much is my total salary in USD in those 10 months.")
